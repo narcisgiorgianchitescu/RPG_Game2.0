7c2ba78b513259cf0d39b8b65f570e61a852ad62
Mihai23I <40754142+Mihai23I@users.noreply.github.com>
2018-07-02 15:21:41 +0300

    Add files via upload

---
+++ Item.rb
@@ -0,0 +1,19 @@
+Dir["lib/**.*"].each { |file| require_relative file }
+
+class Item  
+  def initialize(att=0, defe=0, val=0, nume="noname")  
+     
+    @attack = att 
+    @defence = defe
+	@value = val
+	@name = nume
+  end  
+  
+  def show (a=0) 
+    print "#{@name}: #{@attack} dmg, #{@defence} def"
+	if(a==1)
+		print " - #{@value} coins"
+	end
+  end  
+   
+end
