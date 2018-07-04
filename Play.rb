55d14a2e510516d94df3984d18beb0a65bca3f0a
CatatalinDrg <cdragutescu@stud.acs.upb.ro>
2018-07-04 13:57:41 +0300

    Rubocop compliant
    needs work on case

--- Play.rb
+++ Play.rb
@@ -1,23 +1,23 @@
-require_relative "CustomMap"
-require_relative "RandomMap"
+require_relative 'CustomMap'
+require_relative 'RandomMap'
 
-system 'clear' or system 'cls'
+system('clear') || system('cls')
 
-puts "Enter a hero name"
+puts 'Enter a hero name'
 name = gets.chomp
 hero = Hero.new 50, name, 100, 10, 6
 
-puts "Enter game mode: random/custom"
+puts 'Enter game mode: random/custom'
 choice = gets.chomp
 
 map = nil
 
 loop do
   case choice
-  when "random"
+  when 'random'
     map = RandomMap.new.getmap hero
     break
-  when "custom"
+  when 'custom'
     map = CustomMap.new.getmap hero
     break
   else
