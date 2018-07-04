6112599f84e9dfe1b4c3ab14111b7d3a631264b1
Chitescu Narcis <narcis-giorgian.chitescu@my.fmi.unibuc.ro>
2018-07-04 14:55:17 +0300

    Style + adding _ in show_stats and use_item

--- Character.rb
+++ Character.rb
@@ -1,19 +1,24 @@
+# Character Definition
 class Character
-	attr_accessor :money
-	attr_accessor :hp
-	attr_accessor :attack
-	attr_accessor :defence
-	def initialize(money=0, my_name="noname", hp=100, att=1, defe=1 )
-		@money = money;
-		@name = my_name;
-		@hp = hp;
-		@attack = att;
-		@defence = defe;
-	end
-	def showstats
-		print "Character ", @name,  " has: \n"
-		print @money, " Coins \n"
-		print @hp , " HP\n"
-		print "Attack: ", @attack, "\nDefence: ", @defence, "\n"
-	end
+  attr_accessor :money
+  attr_accessor :hp
+  attr_accessor :attack
+  attr_accessor :defence
+
+  def initialize(money = 0, my_name = 'noname', hitpoints = 100,
+                 attack = 1, defence = 1)
+    @money = money
+    @name = my_name
+    @hp = hitpoints
+    @attack = attack
+    @defence = defence
+  end
+
+  def show_stats
+    print 'Character ', @name, ' has: \n'
+    print @money, ' Coins \n'
+    print @hp, ' HP\n'
+    print 'Attack: ', @attack, '\nDefence: ', @defence, '\n'
+  end
 end
+
