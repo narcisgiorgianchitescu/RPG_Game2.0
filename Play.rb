c898ad484304c32744ca3d2352a243f19ab648df
CatatalinDrg <cdragutescu@stud.acs.upb.ro>
2018-07-05 16:21:57 +0300

    added name to hero

--- Play.rb
+++ Play.rb
@@ -5,7 +5,7 @@ system('clear') || system('cls')
 
 puts 'Enter a hero name'
 name = gets.chomp
-hero = Hero.new
+hero = Hero.new(name: name)
 
 puts 'Enter game mode: random/custom'
 choice = gets.chomp
