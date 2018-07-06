59d5f28af13481dd9eb59954162441451ff028be
Mihai23i <96mihai.ionescu@gmail.com>
2018-07-06 16:26:52 +0300

    Position ++

--- Position.rb
+++ Position.rb
@@ -1,10 +1,11 @@
 require_relative
 
 class Position
-  def initialize(row, column)
+  def initialize(row, column, entity_on_surface)
     @coordinates = [row, column]
+    @entity = entity_on_surface
   end
-  
+
   def move_up
     @coordinates[0] -= 1
   end
