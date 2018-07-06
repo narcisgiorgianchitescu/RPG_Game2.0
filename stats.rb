c5658e6b15734ade9e2c5dc6827200dec3a544f4
CatatalinDrg <cdragutescu@stud.acs.upb.ro>
2018-07-06 15:46:05 +0300

    early work

---
+++ stats.rb
@@ -0,0 +1,26 @@
+# Class that encapsulates the stats that a hero or item can have
+class Stats
+  STATS = {
+    attack:  0,
+    defence: 0,
+    hp:      0,
+    coins:   0,
+  }.freeze
+  attr_accessor :attack
+  attr_accessor :defence
+  attr_accessor :hp
+  attr_accessor :coins
+
+  def initialize(stats = {})
+    STATS.keys.each { |key| send "#{key}=", (stats[key] || 0) }
+  end
+
+  def change_stats(other, sign)
+    STATS.keys.each do |key|
+      old_val = send key.to_s
+      other_val = eval "other.#{key}"
+      new_val = old_val.send(sign, other_val)
+      send "#{key}=", new_val
+    end
+  end
+end
