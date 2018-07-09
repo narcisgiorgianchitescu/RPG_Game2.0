c6eea6946d9eb1ae214de56b87de6d54f0881081
CatatalinDrg <cdragutescu@stud.acs.upb.ro>
2018-07-09 15:40:06 +0300

    item menu - still needs refactoring but should work

--- itemMenu.rb
+++ itemMenu.rb
@@ -1,15 +1,26 @@
+require_relative 'iointerface'
+
 # class that receives a list of options and prompts the io to choose one
 class ItemMenu
-  def initialize(options)
-    @options = options
+  def initialize(items, description, device)
+    @description = description
+    @items = items
+    @device = device
   end
 
   def choice
-    until choice
-      # TODO: send options to io
-      # TODO: get result from io
-      # TODO: check if input is valid
+    input = nil
+    loop do
+      device.print_string description
+      device.next_line
+      @items.each_with_index do |item, index|
+        device.print_string "#{index} "
+        device.print_item item
+        device.next_line
+      end
+      input = device.input
+      break if is_valid? input
     end
-    choice
+    input
   end
 end
