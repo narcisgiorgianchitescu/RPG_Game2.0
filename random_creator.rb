5de76eed650cc1e3632c8d4bacd784aba11e481e
Mihai23i <96mihai.ionescu@gmail.com>
2018-07-09 16:24:13 +0300

    private random_map/creator

--- random_creator.rb
+++ random_creator.rb
@@ -15,23 +15,23 @@ module RandomCreator
     # TODO solution?
   end
 
-  def self.random_map(hero, difficulty = 0)
+  def self.map(hero, difficulty = 0)
     map = Map.new
     map.size.times do |i|
       map.size.times do |j|
-        y = random_room(hero, difficulty)
+        y = room(hero, difficulty)
         map.add_room(y, i, j)
       end
     end
     map = spawn_victory_room(map)
   end
 
-  def self.random_room(hero, difficulty = 0)
+  def self.room(hero, difficulty = 0)
     case rand(1..100)
     when 1..5 then Hospital.new
-    #when 6..29 then MonsterRoom.new(true, random_monster(hero, difficulty))
-    when 30..39 then random_shop(difficulty)
-    when 40..49 then random_vault(difficulty)
+    when 6..29 then MonsterRoom.new(true, monster(hero, difficulty))
+    when 30..39 then shop(difficulty)
+    when 40..49 then vault(difficulty)
     else
       Room.new
     end
@@ -45,52 +45,54 @@ module RandomCreator
     map
   end
 
-  def self.random_item(difficulty = 0)
+  def self.item(difficulty = 0)
     case rand(1..12)
-    when 1..4 then random_consumable(difficulty)
-    when 5..6 then random_head(difficulty)
-    when 7..8 then random_chest(difficulty)
-    when 9..10 then random_boots(difficulty)
-    when 11..12 then random_weapon(difficulty)
+    when 1..4 then consumable(difficulty)
+    when 5..6 then head(difficulty)
+    when 7..8 then chest(difficulty)
+    when 9..10 then boots(difficulty)
+    when 11..12 then weapon(difficulty)
     end
   end
-  def self.random_consumable(difficulty)
-    Consumable.new(random_consumable_stats, 'Random Consumable')
-  end
-  def self.random_consumable_stats(difficulty = 0)
-    Stats.new(random_consumable_stats_hash(difficulty))
+
+  def self.consumable(difficulty = 0)
+    Consumable.new(consumable_stats(difficulty), 'Random Consumable')
   end
 
-  def self.random_consumable_stats_hash(difficulty = 0)
+  def self.consumable_stats(difficulty = 0)
+    Stats.new(consumable_stats_hash(difficulty))
+  end
+
+  def self.consumable_stats_hash(difficulty = 0)
     { attack: (rand(-1..2) * difficulty_multiplier(difficulty)).to_i,
       defence: (rand(-1..2) * difficulty_multiplier(difficulty)).to_i,
       value: (rand(10..30) * difficulty_multiplier(difficulty)).to_i,
       hp: (rand(-5..20) * difficulty_multiplier(difficulty)).to_i}
   end
 
-  def self.random_head(difficulty = 0)
-    Wearable.new(random_stats_equipment(difficulty), :head, 'Random Head')
+  def self.head(difficulty = 0)
+    Wearable.new(stats_equipment(difficulty), :head, 'Random Head')
   end
 
-  def self.random_chest(difficulty = 0)
-    Wearable.new(random_stats_equipment(difficulty), :chest, 'Random Chest')
+  def self.chest(difficulty = 0)
+    Wearable.new(stats_equipment(difficulty), :chest, 'Random Chest')
   end
 
-  def self.random_boots(difficulty = 0)
-    Wearable.new(random_stats_equipment(difficulty), :boots, 'Random Boots')
+  def self.boots(difficulty = 0)
+    Wearable.new(stats_equipment(difficulty), :boots, 'Random Boots')
   end
 
-  def self.random_weapon(difficulty = 0)
-    Weapon.new(random_stats_equipment(difficulty), 'Random Weapon')
+  def self.weapon(difficulty = 0)
+    Weapon.new(stats_equipment(difficulty), 'Random Weapon')
   end
 
-  def self.random_stats_equipment(difficulty = 0)
+  def self.stats_equipment(difficulty = 0)
     Stats.new({ attack: (rand(0..5) * difficulty_multiplier(difficulty)).to_i,
       defence: (rand(0..5) * difficulty_multiplier(difficulty)).to_i,
       value: (rand(10..30) * difficulty_multiplier(difficulty)).to_i })
   end
 
-  def self.random_stats_monster(hero, difficulty = 0)
+  def self.stats_monster(hero, difficulty = 0)
     Stats.new = { money: (rand(15..40) * difficulty_multiplier(difficulty)).to_i,
       hp: ((hero.hp + rand(-10..10)) / difficulty_multiplier(difficulty)).to_i,
       attack: ((hero.attack + rand(-3..1)) / difficulty_multiplier(difficulty)).to_i,
@@ -99,7 +101,7 @@ module RandomCreator
   end
 
 
-  def self.random_monster_name
+  def self.monster_name
     bear_names = ['Angry Bear', 'Mad Bear', 'Stone Bear', 'Grizzly Bear']
     wolf_names = ['Rabid Wold', 'Big Wolf', 'Mad Wolf', 'White Wolf']
     bat_names  = ['Bloodsucking Bat', 'Giant Bat', 'Mutated Bat']
@@ -108,20 +110,23 @@ module RandomCreator
   end
 
 
-  def self.random_monster(hero, difficulty = 0)
-    Monster.new(random_stats_monster(hero, difficulty), random_monster_name, rand(0..100))
+  def self.monster(hero, difficulty = 0)
+    Monster.new(stats_monster(hero, difficulty), monster_name, rand(0..100))
   end
 
 
-  def self.random_shop(difficulty = 0)
+  def self.shop(difficulty = 0)
     items = []
-    rand(3..5).times { items.push(random_item(difficulty)) }
+    rand(3..5).times { items.push(item(difficulty)) }
     Shop.new(true, items)
   end
 
-  def self.random_vault(difficulty = 0)
+  def self.vault(difficulty = 0)
     items = []
-    rand(3..5).to_i.times { items.push(random_item(difficulty)) }
+    rand(3..5).to_i.times { items.push(item(difficulty)) }
     Vault.new(true, items)
   end
+
+  private :spawn_victory_room, :consumable_stats, :consumable_stats_hash
+  private :stats_monster, :stats_equipment, :monster_name
 end
--- random_map.rb
+++ random_map.rb
@@ -4,6 +4,6 @@ require_relative 'random_creator'
 # Generating a random map with different tools
 class RandomMap
   def create_map(hero, difficulty)
-    RandomCreator.random_map(hero, difficulty)
+    RandomCreator.map(hero, difficulty)
   end
 end
