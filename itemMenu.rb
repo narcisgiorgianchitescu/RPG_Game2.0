e2474b8caa3ca8834c1920632b5466099c108e52
CatatalinDrg <cdragutescu@stud.acs.upb.ro>
2018-07-09 15:42:17 +0300

    itemmenu good now

--- itemMenu.rb
+++ itemMenu.rb
@@ -11,16 +11,26 @@ class ItemMenu
   def choice
     input = nil
     loop do
-      device.print_string description
-      device.next_line
-      @items.each_with_index do |item, index|
-        device.print_string "#{index} "
-        device.print_item item
-        device.next_line
-      end
-      input = device.input
+      @device.print_string description
+      @device.next_line
+      print_items
+      input = @device.input
       break if is_valid? input
     end
     input
+  end
+
+  private
+
+  def print_items
+    @items.each_with_index do |item, index|
+      @device.print_string "#{index} "
+      @device.print_item item
+      @device.next_line
+    end
+  end
+
+  def is_valid?(input)
+    input.between(0, @items.size-1)
   end
 end
