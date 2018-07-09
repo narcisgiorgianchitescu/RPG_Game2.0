133a82154abe8a936066fe13ec1cad8b1331b49a
CatatalinDrg <cdragutescu@stud.acs.upb.ro>
2018-07-09 15:16:55 +0300

    added interface methods

--- iointerface.rb
+++ iointerface.rb
@@ -1,9 +1,15 @@
 class IOinterface
   def print_string; end
 
+  def next_line; end
+
   def print_item; end
 
   def print_monster; end
 
   def print_hero; end
+
+  def clear; end
+
+  def input; end
 end
