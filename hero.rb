7e65df022a8602ba15b509acc0e983fd368d666a
Chitescu Narcis <narcis-giorgian.chitescu@my.fmi.unibuc.ro>
2018-07-09 11:54:28 +0300

    added hero

--- hero.rb
+++ hero.rb
@@ -1,35 +1,11 @@
 require_relative 'character.rb'
-
+require_relative 'equipment'
 # Hero Modified
 class Hero < Character
-  def initialize(stats = nil, eq = {}, name = 'noname')
+  attr_accessor :equipement
+  def initialize(stats = nil, equipement = nil, name = 'noname')
     super(stats, name)
-  end
-
-
-  def use_item(item)
-    case item.class.to_s
-    when 'Consumable'
-      @attack += item.attack
-      @defence += item.defence
-      @hp += item.hp
-    when 'Head'
-      equip @head, item
-      @head = item
-    when 'Chest'
-      equip @chest, item
-      @chest = item
-    when 'Weapon'
-      equip @weapon, item
-      @weapon = item
-    when 'Boots'
-      equip @boots, item
-      @boots = item
-    end
-  end
-
-  def equip(old_item, new_item)
-    @attack -= (old_item.attack - new_item.attack)
-    @defence -= (old_item.defence - new_item.defence)
+    equipement ||= Equipment.new
+    @equipement = equipement
   end
 end
