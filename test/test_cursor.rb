08811953c1310fdfc2e61154a098a6724abb7a8b
Mihai23i <96mihai.ionescu@gmail.com>
2018-07-10 15:51:16 +0300

    cursor ; text_cursor -> now "next" returns position over array + update tests for the case

--- test/test_cursor.rb
+++ test/test_cursor.rb
@@ -10,23 +10,60 @@ class TestPosition < Test::Unit::TestCase
   def setup; end
 
   def test_next_up
-    assert_equal([4, 5], Cursor.new(Position.new(5, 5)).next('w'))
-    assert_equal([3, 5], Cursor.new(Position.new(5, 5)).next('up', 2))
+    cursor = Cursor.new(Position.new)
+    assert_equal(
+      cursor.position.row - 1,
+      cursor.next('w').row
+    )
   end
 
   def test_next_down
-    assert_equal([6, 5], Cursor.new(Position.new(5, 5)).next('s'))
-    assert_equal([8, 5], Cursor.new(Position.new(5, 5)).next('down', 3))
+    cursor = Cursor.new(Position.new(5, 5))
+    assert_equal(
+      cursor.position.row + 1,
+      cursor.next('s').row
+    )
+
+    assert_equal(
+      cursor.position.column,
+      cursor.next('s').column
+    )
+    assert_equal(
+      cursor.position.row + 3,
+      cursor.next('down', 3).row
+    )
   end
 
   def test_next_right
-    assert_equal([5, 6], Cursor.new(Position.new(5, 5)).next('d'))
-    assert_equal([5, 9], Cursor.new(Position.new(5, 5)).next('right', 4))
+    cursor = Cursor.new(Position.new(5, 5))
+    assert_equal(
+      cursor.position.row,
+      cursor.move('d').row
+    )
+    assert_equal(
+      cursor.position.column + 1,
+      cursor.move('d').column
+    )
+    assert_equal(
+      cursor.position.column + 3,
+      cursor.move('right', 3).column
+    )
   end
 
   def test_next_left
-    assert_equal([5, 4], Cursor.new(Position.new(5, 5)).next('a'))
-    assert_equal([5, 0], Cursor.new(Position.new(5, 5)).next('left', 5))
+    cursor = Cursor.new(Position.new(5, 5))
+    assert_equal(
+      cursor.position.row,
+      cursor.move('a').row
+    )
+    assert_equal(
+      cursor.position.column - 1,
+      cursor.move('a').column
+    )
+    assert_equal(
+      cursor.position.column - 21,
+      cursor.move('left', 21).column
+    )
   end
 
   def test_move_up
