426d3d5b78380fcbf8c1b78051ca4c88a28a65d9
Chitescu Narcis <narcis-giorgian.chitescu@my.fmi.unibuc.ro>
2018-07-10 14:11:55 +0300

    fix

--- room/monster_room.rb
+++ room/monster_room.rb
@@ -1,6 +1,7 @@
 binpath = File.dirname(__FILE__)
 $LOAD_PATH.unshift File.expand_path(File.join(binpath, '..'))
 require 'require_file'
+require 'monster'
 
 class MonsterRoom < Room
   def initialize(hidden = true, input = Monster.new)
