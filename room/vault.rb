6f022562189a61da90c1eb3fa6021b766f026f36
Unknown <dogarucristian64@yahoo.com>
2018-07-11 12:17:45 +0300

    fixed typos and tests added vault and tests for vault

--- room/vault.rb
+++ room/vault.rb
@@ -1,11 +1,17 @@
 binpath = File.dirname(__FILE__)
 $LOAD_PATH.unshift File.expand_path(File.join(binpath, '..'))
 require 'require_file'
+require 'wearable'
+require 'weapon'
+require 'room'
+require 'hero'
+require 'io_terminal'
 
 class Vault < Room
   attr_accessor :already_chose
+  attr_accessor :got_item
 
-  def initialize(hidden = true, input = [Item.new({}), Item.new({})])
+  def initialize(hidden = true, input = [Wearable.new(), Weapon.new()])
     super(hidden, input)
     @description = 'You can chose one item from this vault'
     @got_item = false
@@ -13,38 +19,50 @@ class Vault < Room
   end
 
   def action(hero)
-    @device.print_string(@already_chose) if @got_item
+    return you_already_chose if @got_item
 
-    prezent_option(hero, true)
+    prezent_option(hero)
   end
 
-  def prezent_option(hero, valid)
-    @device.clear
-    @device.print_string(@description)
-    @device.print_string(@exit_option)
+  def you_already_chose
+    @device.print_string(@already_chose)
 
-    @input.each_with_index do |item, index|
-      @device.print_string((index + @index_correction).to_s)
-      @device.print_item(item)
-    end
-
-    @device.print_string('Invalid input') unless valid
-
-    check_for_valid_input(hero)
-  end
-
-  def check_for_valid_input(hero)
-    valid_input = [0..@input.size]
-    input = @device.input.chomp
-
-    prezent_option(hero, false) unless valid_input === input
-
-    give_item(hero, input) if input > 0
+    @game_on
   end
 
   def give_item(hero, input)
-    hero.use_item(@input[input - @index_correction])
+    give_item_to_hero(hero, input)
 
+    hero_chose_item
+
+    output(hero)
+  end
+
+  def hero_chose_item
     @got_item = true
   end
+
+  def output(hero)
+    return @game_over if hero.stats.hp < 1
+
+    @game_on
+  end
+
+  private
+
+  def prezent_option(hero)
+    menu = get_menu(@description)
+    input = menu.choice
+
+    return @game_on if input == menu.exit_value
+
+    give_item(hero, input)
+  end
 end
+
+# s = Vault.new
+# d = IOterminal.new
+# s.set_device(d)
+# h = Hero.new
+
+# s.action(h)
