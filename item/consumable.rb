44fe49ca4931e146f3e2900b2d0c387e68bc64e1
CatatalinDrg <cdragutescu@stud.acs.upb.ro>
2018-07-12 14:22:41 +0300

    style fix

--- item/consumable.rb
+++ item/consumable.rb
@@ -3,6 +3,7 @@ $LOAD_PATH.unshift File.expand_path(File.join(binpath, '..'))
 require 'require_file'
 require 'item'
 
+# consumable changes hero stats permanently
 class Consumable < Item
   attr_accessor :stats, :name
   def initialize(stats = nil, name = 'Consumable')
