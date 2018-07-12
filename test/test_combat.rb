01d7ab8e563822c4af52677c8116628e751a53ea
CatatalinDrg <cdragutescu@stud.acs.upb.ro>
2018-07-12 10:48:12 +0300

    added tests for combat

--- test/test_combat.rb
+++ test/test_combat.rb
@@ -4,7 +4,61 @@ require 'require_file'
 require 'hero'
 require 'monster'
 require 'minitest/autorun'
+require 'io_interface'
+require 'combat'
 
 class TestCombat < Minitest::Test
-  
+  def setup
+    def rand
+      1
+    end
+
+    def rand n
+      1
+    end
+  end
+
+  def test_monster_dies
+    hero = Hero.new(Stats.new(attack: 1, defence: 1, hp: 1))
+    monster = Monster.new(Stats.new(attack: 0, defence: 0, hp: 1))
+    device = IOinterface.new
+    combat = Combat.new(hero, monster, device)
+    def combat.choices
+      %w[a a]
+    end
+    assert_equal combat.fight, false
+  end
+
+  def test_hero_dies
+    hero = Hero.new(Stats.new(attack: 0, defence: 0, hp: 1))
+    monster = Monster.new(Stats.new(attack: 1, defence: 1, hp: 1))
+    device = IOinterface.new
+    combat = Combat.new(hero, monster, device)
+    def combat.choices
+      %w[a a]
+    end
+    assert_equal combat.fight, true
+  end
+
+  def test_hero_runs
+    hero = Hero.new(Stats.new(attack: 1, defence: 1, hp: 1))
+    monster = Monster.new(Stats.new(attack: 1, defence: 1, hp: 1), '', 2)
+    device = IOinterface.new
+    combat = Combat.new(hero, monster, device)
+    def combat.choices
+      %w[r d]
+    end
+    assert_equal combat.fight, false
+  end
+
+  def test_hero_runs2
+    hero = Hero.new(Stats.new(attack: 1, defence: 1, hp: 1))
+    monster = Monster.new(Stats.new(attack: 1, defence: 1, hp: 1), '', 2)
+    device = IOinterface.new
+    combat = Combat.new(hero, monster, device)
+    def combat.choices
+      %w[r d]
+    end
+    assert_equal monster.alive?, true
+  end
 end
