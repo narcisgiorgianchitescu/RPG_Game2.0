d5a119ba8f092da2a12051a5e5d8f013c59c54a2
Chitescu Narcis <narcis-giorgian.chitescu@my.fmi.unibuc.ro>
2018-07-12 13:40:25 +0300

    sss

--- character/hero.rb
+++ character/hero.rb
@@ -34,28 +34,41 @@ class Hero < Character
 
   def use_consumable(consumable, free = true)
     @stats.change_stats(consumable.stats, :+)
-    @stats.coins -= consumable.stats.coins unless free
+    #@stats.coins -= consumable.stats.coins unless free
   end
 
   def change_wearable(wearable, free = true)
+
     case wearable.type.to_s
     when 'head'
+      @stats.change_stats(@equipment.head.stats, :-)
+      @stats.coins += @equipment.head.stats.coins
       @equipment.head = wearable
       @stats.change_stats(equipment.head.stats, :+)
+      @stats.coins -= @equipment.head.stats.coins
     when 'chest'
+      @stats.change_stats(@equipment.chest.stats, :-)
+      @stats.coins += @equipment.chest.stats.coins
       @equipment.chest = wearable
       @stats.change_stats(equipment.chest.stats, :+)
+      @stats.coins -= @equipment.chest.stats.coins
     else
+      @stats.change_stats(@equipment.boots.stats, :-)
+      @stats.coins += @equipment.boots.stats.coins
       @equipment.boots = wearable
       @stats.change_stats(equipment.boots.stats, :+)
+      @stats.coins -= @equipment.boots.stats.coins
     end
-    @stats.coins -= wearable.stats.coins unless free
+    #@stats.coins -= wearable.stats.coins unless free
   end
 
   def change_weapon(weapon, free = true)
+    @stats.change_stats(@equipment.weapon.stats, :-)
+    @stats.coins += @equipment.weapon.stats.coins
     @equipment.weapon = weapon
     @stats.change_stats(equipment.weapon.stats, :+)
-    @stats.coins -= weapon.stats.coins unless free
+  #  @stats.coins -= weapon.stats.coins unless free
+    @stats.coins += @equipment.weapon.stats.coins
   end
 
   def take_money(amount)
